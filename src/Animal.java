dfa59d6c38ebc3806dc85825ce1a2e565960af39
Sagi Tufman <sagituf@gmail.com>
2021-11-19 21:02:39 +0200

    first commit

--- src/Animal.java
+++ src/Animal.java
@@ -6,7 +6,7 @@ public abstract class Animal {
     private int calsPerMeal;
 
     public Animal(boolean supperPredator, int calsPerMeal) {
-        String[] names = {"Gadi", "Amit", "Daniel", "Benny", "Dario", "Sagi", "Rom", "Ron", "Noy", "Shahar", "Aviram", "Avi", "Shaar", "Shir"};
+        String[] names = {"Dude","Gadi", "Amit", "Daniel", "Benny", "Dario", "Sagi", "Rom", "Ron", "Noy", "Shahar", "Aviram", "Avi", "Shaar", "Shir"};
         this.name = names[(int) (Math.random() * names.length)];
         this.age = (int) (Math.random() * 20) + 1;
         this.supperPredator = supperPredator;
